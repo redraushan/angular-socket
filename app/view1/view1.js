ef0c1ad3557009de5058e686cbb98b07817ebf79
raushan <redraushan@gmail.com>
2016-09-09 20:54:09 +0530

    First Commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute','ngStomp'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -9,6 +9,54 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.factory('socketService',function($stomp,$log){
+    $stomp.setDebug(function (args) {
+        $log.debug(args)
+    });
+
+    var incoming =
+
+    $stomp
+        .connect('http://localhost:8080/stomp')
+
+        // frame = CONNECTED headers
+        .then(function (frame) {
+            var subscription = $stomp.subscribe('/topic/pets', function (message, headers, res) {
+                 incoming = JSON.stringify(message);
+
+            }, {
+                'headers': 'are awesome'
+            });
+        })
+
+     return incoming;
+
+})
+.controller('View1Ctrl', ['$stomp','$log','$scope',function($stomp,$log,$scope) {
+
+    $scope.message = 'Waiting...';
+    $scope.author = 'Raushan';
+
+    $stomp.setDebug(function (args) {
+        $log.debug(args)
+    });
+
+    $stomp
+        .connect('http://localhost:8080/stomp')
+
+        // frame = CONNECTED headers
+        .then(function (frame) {
+            var subscription = $stomp.subscribe('/topic/pets', function (message) {
+                $scope.message = JSON.stringify(message);
+                $scope.$apply();
+
+            }, {
+                'headers': 'are awesome'
+            });
+        })
+
+
+
+
 
 }]);
